9bf9600e17e0d3a5d89b775050610f41bede7e5d
kujo205 <kuc8301@gmail.com>
2023-11-13 01:52:55 +0200

    feature: added user roles and message types

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -3,11 +3,11 @@
 
 import { sql } from "drizzle-orm";
 import {
-  bigint,
-  index,
-  mysqlTableCreator,
-  timestamp,
-  varchar,
+    bigint,
+    index,
+    mysqlTableCreator,
+    timestamp,
+    varchar, mysqlEnum, boolean,
 } from "drizzle-orm/mysql-core";
 import { relations } from 'drizzle-orm';
 /**
@@ -20,9 +20,14 @@ export const mysqlTable = mysqlTableCreator((name) => `anochat_${name}`);
 
 
 
+
 export const user=mysqlTable('user',{
     id:bigint('id',{mode:'number'}).primaryKey().autoincrement(),
     name:varchar('name',{length:256}).notNull(),
+    description:varchar('description',{length:500}).default('No description'),
+    avatar:varchar('avatar',{length:256}),
+    accessKey:varchar('access_key',{length:256}),
+    isInQueue:boolean('is_in_queue').default(false),
     createdAt:timestamp('created_at').default(sql`CURRENT_TIMESTAMP`).notNull(),
     updatedAt:timestamp('updated_at').onUpdateNow(),
 })
@@ -35,8 +40,10 @@ export const userRelations = relations(user, ({ many }) => ({
 
 export const room=mysqlTable(
     'room',{
+        creatorId:bigint('creator_id',{mode:'number'}).notNull(),
         id:bigint('id',{mode:'number'}).notNull().primaryKey().autoincrement(),
         name:varchar('name',{length:256}).notNull(),
+        type:mysqlEnum('group_type', ['PERSONAL', 'DUAL', 'MULTIPLE']),
         createdAt:timestamp('created_at').default(sql`CURRENT_TIMESTAMP`).notNull(),
         updatedAt:timestamp('updated_at').onUpdateNow(),
     }
@@ -46,11 +53,14 @@ export const roomRelations=relations(room,({many})=>({
     users:many(user)
 }))
 
-
+export const role=mysqlTable('role',{
+    id:bigint('id',{mode:'number'}).notNull().primaryKey().autoincrement(),
+    name:varchar('role', {length:150}).notNull(),
+})
 
 export const roomToUser = mysqlTable('room_to_user', {
         id: bigint('user_id', {mode: 'number'}).notNull().primaryKey().autoincrement(),
-        ownerId:bigint('creatorId_id',{mode:'number'}).notNull(),
+        roleId:bigint('role_id', {mode: 'number'}).notNull(),
         userId: bigint('user_id', {mode: 'number'}).notNull(),
         roomId: bigint('room_id', {mode: 'number'}).notNull(),
         createdAt: timestamp('created_at').default(sql`CURRENT_TIMESTAMP`).notNull(),
@@ -68,6 +78,10 @@ export const roomToUserRelations=relations(roomToUser,({one})=>({
         fields:[roomToUser.roomId],
         references:[room.id]
     }),
+    roleId:one(role,{
+        fields:[roomToUser.roleId],
+        references:[role.id]
+    })
 }))
 
 
@@ -78,6 +92,8 @@ export const message=mysqlTable('message',{
     roomId:bigint('room_id',{mode:'number'}).notNull(),
     createdAt:timestamp('created_at').default(sql`CURRENT_TIMESTAMP`).notNull(),
     updatedAt:timestamp('updated_at').onUpdateNow(),
+    type:mysqlEnum('message_type', ['TEXT', 'IMAGE']),
+    url:varchar('url',{length:256}),
 })
 
 export const messageRelations=relations(message,({one})=>({
@@ -91,21 +107,3 @@ export const messageRelations=relations(message,({one})=>({
     })
 }))
 
-
-
-
-
-export const posts = mysqlTable(
-  "post",
-  {
-    id: bigint("id", { mode: "number" }).primaryKey().autoincrement(),
-    name: varchar("name", { length: 256 }),
-    createdAt: timestamp("created_at")
-      .default(sql`CURRENT_TIMESTAMP`)
-      .notNull(),
-    updatedAt: timestamp("updatedAt").onUpdateNow(),
-  },
-  (example) => ({
-    nameIndex: index("name_idx").on(example.name),
-  })
-);
